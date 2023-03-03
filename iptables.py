7edfb82f7958855e5cdf90931cf43a46f82d744e
Maria Duvert <maria.duvert@hotmail.com>
2023-03-03 15:54:32 +0000

    ajout css et debut squelette html

--- iptables.py
+++ iptables.py
@@ -1,28 +1,29 @@
 # save this as app.py
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return render_template("index.html")
 
 @app.route("/start")
-def hello():
-    return "Hello, World!"
+def start():
+    return render_template("accueil.html")
 
 @app.route("/rules_filter")
-def hello():
-    return "Hello, World!"
+def rules_filter():
+    return "rules_filter methode"
 
 @app.route("/rules_nat")
-def hello():
-    return "Hello, World!"
+def rules_nat():
+    return render_template("reglesNAT.html")
 
 @app.route("/rules_nat_add")
-def hello():
-    return "Hello, World!"
+def rules_nat_add():
+    return render_template("ajouterNAT.html")
 
 @app.route("/alias")
-def hello():
-    return "Hello, World!"
+def alias():
+    return render_template("alias.html")
